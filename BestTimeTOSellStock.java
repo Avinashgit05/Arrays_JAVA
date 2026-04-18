342f9747d89832ac70db8e0db9a29b39673d2841
Avinash <avinash649556@gmail.com>
2026-04-18 21:04:07 +0530

    Optimized Code

--- BestTimeTOSellStock.java
+++ BestTimeTOSellStock.java
@@ -1,13 +1,13 @@
 public class BestTimeTOSellStock {
     public static int maxProfit(int[] prices) {
+        int lowestPrice=prices[0];
         int maxProfit=0;
-        for(int i =0; i<prices.length;i++){
-            for(int j=i;j<prices.length;j++){
-                if(prices[j]>prices[i]){
-                    int profit=prices[j]-prices[i];
-                    maxProfit=Math.max(maxProfit,profit);
-                }
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<lowestPrice){
+                lowestPrice=prices[i];
             }
+            int profit=prices[i]-lowestPrice;
+            maxProfit =Math.max(maxProfit,profit);
         }
         return maxProfit;
     }
