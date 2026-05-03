f740025bc66f4e69d6aeb38d6beb9465b3290a23
Avinash <avinash649556@gmail.com>
2026-05-03 16:09:13 +0530

    Optimized

--- MaximumAverageSubarray1.java
+++ MaximumAverageSubarray1.java
@@ -1,11 +1,23 @@
 public class MaximumAverageSubarray1 {
-     public static  double findMaxAverage(int[] nums, int k) {
+     public static  double findMaxAverageBruteForce(int[] nums, int k) {
         double max=Double.NEGATIVE_INFINITY;
         for(int i=0;i<=nums.length-k;i++){
             int sum=0;
             for(int j=i; j<i+k;j++){
                 sum+=nums[j];
             }
+            max=Math.max(max,(double)sum/k);
+        }
+        return max;
+    }
+    public static double findMaxAverage(int[] nums, int k) {
+        int sum=0;
+        for(int i=0; i<k;i++){
+            sum+=nums[i];
+        }
+        double max=(double)sum/k;
+        for(int j=k; j<nums.length;j++){
+            sum=sum-nums[j-k]+nums[j];
             max=Math.max(max,(double)sum/k);
         }
         return max;
