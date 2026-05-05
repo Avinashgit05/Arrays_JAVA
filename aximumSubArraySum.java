2202d0c1efd9484c1221ef1dee97bb216d75afdd
Avinash <avinash649556@gmail.com>
2026-05-05 10:45:42 +0530

    Optimized

--- aximumSubArraySum.java
+++ aximumSubArraySum.java
@@ -1,13 +1,13 @@
 public class aximumSubArraySum {
-    // public static int maxSubArray(int[] nums) {
-    //     int max=nums[0];
-    //     int currMax=nums[0];
-    //     for(int i=1; i<nums.length;i++){
-    //          currMax=Math.max(nums[i], currMax+nums[i]);
-    //          max=Math.max(max,currMax);
-    //     }
-    //     return max;
-    // }
+    public static int maxSubArray(int[] nums) {
+        int max=nums[0];
+        int currMax=nums[0];
+        for(int i=1; i<nums.length;i++){
+             currMax=Math.max(nums[i], currMax+nums[i]);
+             max=Math.max(max,currMax);
+        }
+        return max;
+    }
     public static int maxSubArrayBruteForce(int[] nums){
         int max=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
@@ -21,7 +21,7 @@ public class aximumSubArraySum {
     }
     public static void main(String[] args) {
         int nums[]={-2,1,-3,4,-1,2,1,-5,4};
-        // System.out.println(maxSubArray( nums));
+        System.out.println(maxSubArray( nums));
         System.out.println(maxSubArrayBruteForce(nums));
     }
     
