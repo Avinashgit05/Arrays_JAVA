e3a42eb7c4d063428739fe707397b7164f5072e9
Avinash <avinash649556@gmail.com>
2026-05-11 21:29:30 +0530

    Quick Sort

---
+++ QuickSort.java
@@ -0,0 +1,9 @@
+public class QuickSort {
+    public static void main(String[] args) {
+        int [] nums={10,50,30,20,40,70};
+        for(int num : nums){
+            System.out.print(num+" ");
+        }
+    }
+    
+}
