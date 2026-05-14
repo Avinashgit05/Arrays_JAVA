053c8337b5b1b9bd868bacdf967fe9865f81925b
Avinash <avinash649556@gmail.com>
2026-05-14 20:12:38 +0530

    Find Peak element

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -15,8 +15,8 @@ public class FindPeakElement {
     }
 
     public static void main(String[] args) {
-        int nums[]={1,2,1,3,5,6,4};
-        System.out.println(findPeakElement(nums));
+        int arr[]={1,2,1,3,5,6,4};
+        System.out.println(findPeakElement(arr));
     }
     
 }
