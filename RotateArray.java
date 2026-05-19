d3173cf0cdfa248c549e98b9e5bfa36405e68e60
Avinash <avinash649556@gmail.com>
2026-05-19 22:14:05 +0530

    Rotate Array

--- RotateArray.java
+++ RotateArray.java
@@ -29,6 +29,7 @@ public class RotateArray {
     public static void main(String[] args) {
         int nums[]={1,2,3,4,5,6,7};
         int k=3;
+        // optimized call.
         rotate(nums,k);
         for(int num :nums){
             System.out.print(num+" ");
