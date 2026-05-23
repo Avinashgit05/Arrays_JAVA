13df7e89e0b32b38d93bc00ea8b7a3f43a294e13
Avinash <avinash649556@gmail.com>
2026-05-23 16:14:29 +0530

    Check Single Number

---
+++ CheckSingleNumber.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+public class CheckSingleNumber {
+    public static boolean isDistinct(int[] arr) {
+        HashMap<Integer, Boolean> map=new HashMap<>();
+        for(int val : arr){
+            if( map.get(val)==null){
+                map.put(val,true);
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        int nums[]={1, 2, 3, 4, 7};
+        System.out.println(isDistinct(nums));
+    }
+}
