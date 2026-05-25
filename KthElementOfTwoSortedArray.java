b39718a0f642daad42f7b3c0693c1afd238151c2
Avinash <avinash649556@gmail.com>
2026-05-25 22:00:09 +0530

    Kth Element of Two Sorted Array

---
+++ KthElementOfTwoSortedArray.java
@@ -0,0 +1,30 @@
+public class KthElementOfTwoSortedArray {
+     public static int kthElement(int a[], int b[], int k) {
+          int m=a.length;
+        int n=b.length;
+        int result[]=new int[n+m];
+        int i=0;
+        int j=0;
+        int l=0;
+        while(i<m && j<n){
+            if(a[i]<b[j]){
+                result[l++]=a[i++];
+            }else{
+                result[l++]=b[j++];
+            }
+        }
+        while(i<m){
+            result[l++]=a[i++];
+        }
+        while(j<n){
+            result[l++]=b[j++];
+        }
+        return result[k-1];
+    }
+    public static void main(String args[]){
+        int a[] = {2, 3, 6, 7, 9};
+        int b[] = {1, 4, 8, 10};
+        int k = 5;
+        System.out.println(kthElement(a, b, k));
+    }
+}
