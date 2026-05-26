0cf2aa97b1caae15ecd5b9d906fe27e3a34bf538
Avinash <avinash649556@gmail.com>
2026-05-26 08:44:51 +0530

    Kth Element of Two sorted array

--- KthElementOfTwoSortedArray.java
+++ KthElementOfTwoSortedArray.java
@@ -6,6 +6,7 @@ public class KthElementOfTwoSortedArray {
         int i=0;
         int j=0;
         int l=0;
+        // merge two array 
         while(i<m && j<n){
             if(a[i]<b[j]){
                 result[l++]=a[i++];
@@ -13,12 +14,15 @@ public class KthElementOfTwoSortedArray {
                 result[l++]=b[j++];
             }
         }
+        // if the element left in a then insert
         while(i<m){
             result[l++]=a[i++];
         }
+        // if the element left in b then insert
         while(j<n){
             result[l++]=b[j++];
         }
+        // k-1th element will be the kth element in newly formed array 
         return result[k-1];
     }
     public static void main(String args[]){
