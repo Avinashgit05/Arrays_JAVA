32848d5b9c265fd0659176e1eb13af910a416eee
Avinash <avinash649556@gmail.com>
2026-05-30 20:43:48 +0530

    Remove Duplicats

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -2,6 +2,7 @@ public class RemoveDuplicates {
     public static int removeDuplicates(int[] nums) {
         int writer =0;
         for(int reader=1; reader<nums.length;reader++){
+            // if the duplicate is found 
            if(nums[writer]!=nums[reader]){
                 writer++;
                 nums[writer]=nums[reader];
