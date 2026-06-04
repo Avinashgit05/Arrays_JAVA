2c2830f2ca8e08c980fe0ce7a210b3b3065e33b9
Avinash <avinash649556@gmail.com>
2026-06-04 11:30:01 +0530

    Rotate Image 2D Array

---
+++ RotateImage.java
@@ -0,0 +1,35 @@
+public class RotateImage {
+    public static int[][] rotate(int[][] matrix) {
+        int n= matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            int left =0;
+            int right=n-1;
+            while(left<right){
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+            }
+        }
+        return matrix;
+    }
+    public static void main(String arga[]){
+        int matrix[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int result[][]=rotate(matrix);
+        for(int row[] : result){
+            for(int ele: row){
+                System.out.print(ele+" ");
+            }
+            System.out.println();
+        }
+    }
+}
